813a54e1cf2929752c98017604cd373ea41b7e61
Ray <ray@RaydeMac-mini.local>
2026-04-11 21:47:22 +0800

    chore: set vercel base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "zh-CN",
-    baseUrl: "localhost",
+    baseUrl: "blog-chengliang1s-projects.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
